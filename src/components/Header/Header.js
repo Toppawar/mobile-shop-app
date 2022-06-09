bb1c56174ae1cd08eda7aa59e3b59f17475104d3
lonewar <pablo.divergency@gmail.com>
2022-06-09 10:10:21 +0200

    chore: reestructure package.json

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import { useNavigate, useParams, useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import {
   Box,
   Flex,
